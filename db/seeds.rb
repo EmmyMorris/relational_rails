5b48ae951e16a6a160377cdac39046ce7123b881
Sidarth Bagawandoss <sidarth20@gmail.com>
2021-05-11 20:58:56 -0600

    Reseed databases & reorder id's

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# hospital1 = Hospital.create!([{ name: 'St. Lukes'}, {max_capacity: false}, {beds: 300}])
-# hospital2 = Hospital.create([{ name: 'Sky Ridge'}, {max_capacity: true}, {beds: 200}])
-# hospital3 = Hospital.create([{ name: 'Denver Health'}, {max_capacity: false}, {beds: 600}])
-#
-# patient1 = Patient.create([{name: 'Mike'}, {visitors: true}, {age: 37}])
-# patient2 = Patient.create([{name: 'Megan'}, {visitors: true}, {age: 31}])
-# patient3 = Patient.create([{name: 'Bob'}, {visitors: false}, {age: 29}])
-#
-# clinic1 = Clinic.create([{name: 'First Priority'}, {open: false}, {computers: 80}])
-# clinic2 = Clinic.create([{name: 'Medical Plus'}, {open: true}, {computers: 100}])
-# clinic3 = Clinic.create([{name: 'Healing Helpers'}, {open: true}, {computers: 70}])
-#
-# doctor1 = Doctor.create([{name: 'Brian'}, {available: true}, {rating: 3.5}])
-# doctor2 = Doctor.create([{name: 'Jamison'}, {available: false}, {rating: 4.7}])
-# doctor3 = Doctor.create([{name: 'Ian'}, {available: true}, {rating: 4.0}])
+Hospital.destroy_all
+hospital = Hospital.create!(name: 'St. Lukes', max_capacity: false, beds: 300)
+Hospital.create!(name: 'Sky Ridge', max_capacity: true, beds: 200)
+Hospital.create!(name: 'Denver Health', max_capacity: false, beds: 600)
+
+patient = Patient.create!(name: 'Mike', visitors: true, age: 37)
+Patient.create!(name: 'Megan', visitors: true, age: 31)
+Patient.create!(name: 'Bob', visitors: false, age: 29)
+
+clinic = Clinic.create!(name: 'First Priority', open: false, computers: 80)
+Clinic.create!(name: 'Medical Plus', open: true, computers: 100)
+Clinic.create!(name: 'Healing Helpers', open: true, computers: 70)
+
+doctor = Doctor.create!(name: 'Brian', available: true, rating: 3.5)
+Doctor.create!(name: 'Jamison', available: false, rating: 4.7)
+Doctor.create!(name: 'Ian', available: true, rating: 4.0)
