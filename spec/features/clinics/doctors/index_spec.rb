5d067095d6e220e09d715f90813dd9f1f7cd756d
Sidarth Bagawandoss <sidarth20@gmail.com>
2021-05-13 15:57:17 -0600

    Update user story 5:

--- spec/features/clinics/doctors/index_spec.rb
+++ spec/features/clinics/doctors/index_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'Clinics doctors index' do
   it 'shows all of the doctors associated with that Clinic with each Doctors attributes' do
+    # User Story 5
+    # As a visitor
+    # When I visit '/parents/:parent_id/child_table_name'
+    # Then I see each Child that is associated with that Parent with each Child's attributes:
     clinic = Clinic.create!(name: 'First Priority', open: false, computers: 80)
     brian = clinic.doctors.create!(name: 'Brian', available: true, rating: 3.5)
     george = clinic.doctors.create!(name: 'George', available: true, rating: 3.0)
