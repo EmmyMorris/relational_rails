4bbc833b508ff70d84aad67c017a36f73f30ae81
Sidarth Bagawandoss <sidarth20@gmail.com>
2021-05-18 14:18:26 -0600

    Attempt at user_story_19 clinic

---
+++ spec/features/clinics/delete_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "clinic show page", type: :feature do
+  it "can delete the clinic from the show page" do
+    # User Story 19, Parent Delete (x2)
+    # As a visitor
+    # When I visit a parent show page
+    # Then I see a link to delete the parent
+    # When I click the link "Delete Parent"
+    # Then a 'DELETE' request is sent to '/parents/:id',
+    # the parent is deleted, and all child records are deleted
+    # and I am redirected to the parent index page where I no longer see this parent
+
+    clinic_1 = Clinic.create!(name: 'First Priority', open: false, computers: 80)
+
+    visit "/clinics/#{clinic_1.id}"
+    click_on "Delete Clinic #{clinic_1.name}"
+
+    expect(current_path).to eq ("/clinics/#{clinic_1.id}")
+    expect(page).not_to have_content('First Priority')
+  end
+end
