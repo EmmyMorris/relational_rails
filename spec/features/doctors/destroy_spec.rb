3ebb5f78169ed79da4d6da70d3e8d680f881805f
Sidarth Bagawandoss <sidarth20@gmail.com>
2021-05-18 18:17:28 -0600

    Create feature test for user story 23 doctor

--- spec/features/doctors/destroy_spec.rb
+++ spec/features/doctors/destroy_spec.rb
@@ -20,4 +20,22 @@ RSpec.describe "doctor show page", type: :feature do
     expect(current_path).to eq ("/doctors")
     expect(page).not_to have_content('Brian')
   end
+
+  it "can delete the doctor from the index page" do
+    # User Story 23, Child Delete From Childs Index Page (x1)
+    # As a visitor
+    # When I visit the `child_table_name` index page or a parent `child_table_name` index page
+    # Next to every child, I see a link to delete that child
+    # When I click the link
+    # I should be taken to the `child_table_name` index page where I no longer see that child
+
+    clinic = Clinic.create!(name: 'First Priority', open: false, computers: 80)
+    doctor = clinic.doctors.create!(name: 'Brian', available: true, rating: 3.5)
+
+    visit "/doctors"
+    click_on "Delete Doctor #{doctor.name}"
+
+    expect(current_path).to eq ("/doctors")
+    expect(page).not_to have_content('Brian')
+  end
 end
